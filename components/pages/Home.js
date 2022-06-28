d17ed19cbb3890554c3602796ce4bd76f4bf97ac
Nathan LEMALE <nath44.lemale@gmail.com>
2022-06-28 23:37:13 +0200

    [MOD] add logo picture, home page update with picture and containers

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, Image, TouchableOpacity, View } from "react-native";
 
 export default function ContactScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "darkturquoise" }}>
-      <Text>Home!</Text>
+    <View style={[styles.container, {
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center', backgroundColor: "yellow", flexDirection: "row"}}>
+        <Image
+          source={require('../../assets/portrait.png')}
+          style={{ flex: 3, justifyContent: 'center', alignItems: 'center', width: 200, height: 200 }}
+        />
+        <Image
+          source={require('../../assets/french.png')}
+          style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-end', width: 40, height: 40 }}
+        />
+      </View>
+    <View style={{ flex: 7, backgroundColor: "green" }}>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
   },
 });
 
