1fe5097d33ec75cd7beeadf7f63220fbba1503ee
Nathan LEMALE <nath44.lemale@gmail.com>
2022-07-14 23:09:36 +0200

    [MOD] update home âge a bit

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -11,21 +11,22 @@ export default function ContactScreen() {
       <View style={{ flex: 3, backgroundColor: theme.darkGrey, flexDirection: "row"}}>
         <View style={{ justifyContent: 'center', alignItems: 'flex-start', paddingTop: 40, flex: 1, flexDirection: "row"}}>
         </View>
-        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'flex-end', flexDirection: "column"}}>
+        <View style={{ paddingTop: 50, flex: 2, justifyContent: 'center', alignItems: 'flex-end', flexDirection: "column"}}>
           <Image
             source={require('../../assets/portrait.png')}
             style={{ width: 200, height: 200 }}
           />
         </View>
-        <View style={{ justifyContent: 'center', alignItems: 'flex-start', paddingTop: 40, flex: 1, flexDirection: "row"}}>
+        <View style={{ justifyContent: 'center', alignItems: 'flex-start', paddingTop: 60, flex: 1, flexDirection: "row"}}>
           <Image
             source={require('../../assets/french.png')}
             style={{ width: 40, height: 40 }}
           />
         </View>
       </View>
-      <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.lightGrey}}>
-        <Text style={{}}>Hello There!</Text>
+      <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.darkGrey}}>
+        <Text style={[styles.text, {}]}>Hello There!</Text>
+        <Text style={[styles.text, {}]}>My name is Nathan, i'm a french 19yo developer!</Text>
       </View>
     </View>
 
@@ -36,5 +37,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
 
